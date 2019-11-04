6ed81484127907762dd6a039185172ab51849b5f
yangxin <1393003255@qq.com>
2019-11-04 15:52:47 +0800

    feat: MaxDepth

---
+++ src/main/java/org/yangxin/test/leetcode/MaxDepth.java
@@ -0,0 +1,44 @@
+package org.yangxin.test.leetcode;
+
+/**
+ * 二叉树的最大深度
+ *
+ * @author yangxin
+ * 2019/11/04 15:52
+ */
+public class MaxDepth {
+    public static void main(String[] args) {
+        TreeNode node1 = new TreeNode(3);
+        TreeNode node2 = new TreeNode(9);
+        TreeNode node3 = new TreeNode(20);
+        TreeNode node4 = new TreeNode(15);
+        TreeNode node5 = new TreeNode(7);
+        node1.left = node2;
+        node1.right = node3;
+        node3.left = node4;
+        node3.right = node5;
+        int result = maxDepth(node1);
+        System.out.println(result);
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    private static int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+}
