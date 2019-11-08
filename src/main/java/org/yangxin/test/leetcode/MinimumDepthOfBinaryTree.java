81bc4ccbdb67a43a7712144b39260353a6ade858
yangxin <1393003255@qq.com>
2019-11-08 10:08:39 +0800

    feat: MinimumDepthOfBinaryTree

---
+++ src/main/java/org/yangxin/test/leetcode/MinimumDepthOfBinaryTree.java
@@ -0,0 +1,55 @@
+package org.yangxin.test.leetcode;
+
+/**
+ * 二叉树的最小深度
+ *
+ * @author yangxin
+ * 2019/11/08 09:51
+ */
+public class MinimumDepthOfBinaryTree {
+    public static void main(String[] args) {
+        TreeNode node1 = new TreeNode(3);
+        TreeNode node2 = new TreeNode(9);
+        TreeNode node3 = new TreeNode(20);
+        TreeNode node4 = new TreeNode(15);
+        TreeNode node5 = new TreeNode(7);
+        node1.left = node2;
+        node1.right = node3;
+        node3.left = node4;
+        node3.right = node5;
+
+        TreeNode node6 = new TreeNode(1);
+        node6.right = new TreeNode(2);
+        int result = minDepth(node6);
+        System.out.println(result);
+    }
+
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    private static int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+
+        if (root.left == null) {
+            return minDepth(root.right) + 1;
+        }
+
+        if (root.right == null) {
+            return minDepth(root.left) + 1;
+        }
+
+        return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
+    }
+}
