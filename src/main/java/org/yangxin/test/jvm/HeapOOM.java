ca2b2a709b4dd43436772622f20f41186507b618
yangxin <1393003255@qq.com>
2019-11-12 11:56:32 +0800

    feat: 堆内存溢出

---
+++ src/main/java/org/yangxin/test/jvm/HeapOOM.java
@@ -0,0 +1,24 @@
+package org.yangxin.test.jvm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 堆内存溢出
+ * -Xms20m -Xmx20m -XX:+HeapDumpOnOutOfMemoryError
+ *
+ * @author yangxin
+ * 2019/11/01 11:22
+ */
+public class HeapOOM {
+    static class OOMObject {
+
+    }
+
+    public static void main(String[] args) {
+        List<OOMObject> list = new ArrayList<>();
+        while (true) {
+            list.add(new OOMObject());
+        }
+    }
+}
