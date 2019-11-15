176559c226cc3600ac2950d67edbce45d37b41b3
yangxin <1393003255@qq.com>
2019-11-15 17:57:37 +0800

    feat: JobDataMap中，对key的值的处理是大小写不敏感的

--- src/main/java/org/yangxin/test/quartz/JobTest.java
+++ src/main/java/org/yangxin/test/quartz/JobTest.java
@@ -1,5 +1,6 @@
 package org.yangxin.test.quartz;
 
+import lombok.Data;
 import org.quartz.*;
 
 import java.text.SimpleDateFormat;
@@ -11,7 +12,12 @@ import java.util.Date;
  * @author yangxin
  * 2019/11/15 17:03
  */
+@Data
 public class JobTest implements Job {
+    private String message;
+    private Float floatJobValue;
+    private Double doubleTriggerValue;
+
     @Override
     public void execute(JobExecutionContext jobExecutionContext) {
         // 打印当前的执行时间，格式为2017-01-01 00:00:00
@@ -29,16 +35,17 @@ public class JobTest implements Job {
         System.out.println(triggerKey.getName() + ":" + triggerKey.getGroup());
 
         // JobDataMap(JobDetail)
-        JobDataMap jobDetailJobDataMap = jobExecutionContext.getJobDetail().getJobDataMap();
-        JobDataMap triggerJobDataMap = jobExecutionContext.getTrigger().getJobDataMap();
-        String jobDetailMessage = jobDetailJobDataMap.getString("message");
-        float floatJobValue = jobDetailJobDataMap.getFloat("FloatJobValue");
-        String triggerMessage = triggerJobDataMap.getString("message");
-        double doubleTriggerValue = triggerJobDataMap.getDouble("DoubleTriggerValue");
+//        JobDataMap jobDetailJobDataMap = jobExecutionContext.getJobDetail().getJobDataMap();
+//        JobDataMap triggerJobDataMap = jobExecutionContext.getTrigger().getJobDataMap();
+//        JobDataMap mergedJobDataMap = jobExecutionContext.getMergedJobDataMap();
+//        String message = mergedJobDataMap.getString("message");
+//        float floatJobValue = mergedJobDataMap.getFloat("FloatJobValue");
+//        String triggerMessage = mergedJobDataMap.getString("message");
+//        double doubleTriggerValue = mergedJobDataMap.getDouble("DoubleTriggerValue");
 
-        System.out.println(jobDetailMessage);
+        System.out.println(message);
         System.out.println(floatJobValue);
-        System.out.println(triggerMessage);
+//        System.out.println(triggerMessage);
         System.out.println(doubleTriggerValue);
     }
 }
