9ad071a8c02136b8453623fb357e7dc0e4aeee7a
yangxin <1393003255@qq.com>
2019-11-15 18:09:52 +0800

    feat: Trigger设置起始时间、终止时间

--- src/main/java/org/yangxin/test/quartz/JobTest.java
+++ src/main/java/org/yangxin/test/quartz/JobTest.java
@@ -24,28 +24,28 @@ public class JobTest implements Job {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         System.out.println("Current Exec Time Is: " + simpleDateFormat.format(new Date()));
 
-        // 编写具体的业务逻辑
-//        System.out.println("Hello World!");
-        // Job
-        JobKey jobKey = jobExecutionContext.getJobDetail().getKey();
-        System.out.println(jobKey.getName() + ":" + jobKey.getGroup());
-
-        // Trigger
-        TriggerKey triggerKey = jobExecutionContext.getTrigger().getKey();
-        System.out.println(triggerKey.getName() + ":" + triggerKey.getGroup());
-
-        // JobDataMap(JobDetail)
-//        JobDataMap jobDetailJobDataMap = jobExecutionContext.getJobDetail().getJobDataMap();
-//        JobDataMap triggerJobDataMap = jobExecutionContext.getTrigger().getJobDataMap();
-//        JobDataMap mergedJobDataMap = jobExecutionContext.getMergedJobDataMap();
-//        String message = mergedJobDataMap.getString("message");
-//        float floatJobValue = mergedJobDataMap.getFloat("FloatJobValue");
-//        String triggerMessage = mergedJobDataMap.getString("message");
-//        double doubleTriggerValue = mergedJobDataMap.getDouble("DoubleTriggerValue");
-
-        System.out.println(message);
-        System.out.println(floatJobValue);
-//        System.out.println(triggerMessage);
-        System.out.println(doubleTriggerValue);
+//        // 编写具体的业务逻辑
+////        System.out.println("Hello World!");
+//        // Job
+////        JobKey jobKey = jobExecutionContext.getJobDetail().getKey();
+////        System.out.println(jobKey.getName() + ":" + jobKey.getGroup());
+////
+////        // Trigger
+////        TriggerKey triggerKey = jobExecutionContext.getTrigger().getKey();
+////        System.out.println(triggerKey.getName() + ":" + triggerKey.getGroup());
+//
+//        // JobDataMap(JobDetail)
+////        JobDataMap jobDetailJobDataMap = jobExecutionContext.getJobDetail().getJobDataMap();
+////        JobDataMap triggerJobDataMap = jobExecutionContext.getTrigger().getJobDataMap();
+////        JobDataMap mergedJobDataMap = jobExecutionContext.getMergedJobDataMap();
+////        String message = mergedJobDataMap.getString("message");
+////        float floatJobValue = mergedJobDataMap.getFloat("FloatJobValue");
+////        String triggerMessage = mergedJobDataMap.getString("message");
+////        double doubleTriggerValue = mergedJobDataMap.getDouble("DoubleTriggerValue");
+//
+//        System.out.println(message);
+//        System.out.println(floatJobValue);
+////        System.out.println(triggerMessage);
+//        System.out.println(doubleTriggerValue);
     }
 }
--- src/main/java/org/yangxin/test/quartz/SchedulerTest.java
+++ src/main/java/org/yangxin/test/quartz/SchedulerTest.java
@@ -14,11 +14,23 @@ import java.util.Date;
  */
 public class SchedulerTest {
     public static void main(String[] args) {
+        // 打印当前的执行时间，格式为2017-01-01 00:00:00
+        Date currentTime = new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.println("Current Time Is: " + simpleDateFormat.format(currentTime));
+
+        // 开始时间
+        Date startTime = new Date();
+        startTime.setTime(currentTime.getTime() + 3000);
+        // 结束时间
+        Date endTime = new Date();
+        endTime.setTime(currentTime.getTime() + 6000);
+
         // 创建一个JobDetail实例，将该实例与JobTest.class绑定
         JobDetail jobDetail = JobBuilder.newJob(JobTest.class)
                 .withIdentity("job1", "group1")
-                .usingJobData("message", "job1")
-                .usingJobData("FloatJobValue", 3.14F)
+//                .usingJobData("message", "job1")
+//                .usingJobData("FloatJobValue", 3.14F)
                 .build();
 
         System.out.println("jobDetail's name: " + jobDetail.getKey().getName());
@@ -28,12 +40,13 @@ public class SchedulerTest {
         // 创建一个Trigger实例，定义该job立即执行，并且每隔两秒钟重复执行一次，知道程序手动终止
         Trigger trigger = TriggerBuilder.newTrigger()
                 .withIdentity("trigger1", "group1")
-                .startNow()
+                .startAt(startTime)
+                .endAt(endTime)
                 .withSchedule(SimpleScheduleBuilder.simpleSchedule()
                         .withIntervalInSeconds(2)
                         .repeatForever())
-                .usingJobData("message", "trigger1")
-                .usingJobData("DoubleTriggerValue", 2.0D)
+//                .usingJobData("message", "trigger1")
+//                .usingJobData("DoubleTriggerValue", 2.0D)
                 .build();
 
         // 创建Scheduler实例
@@ -42,9 +55,9 @@ public class SchedulerTest {
             Scheduler scheduler = stdSchedulerFactory.getScheduler();
             scheduler.start();
 
-            // 打印当前的执行时间，格式为2017-01-01 00:00:00
-            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            System.out.println("Current Time Is: " + simpleDateFormat.format(new Date()));
+//            // 打印当前的执行时间，格式为2017-01-01 00:00:00
+//            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//            System.out.println("Current Time Is: " + simpleDateFormat.format(new Date()));
 
             scheduler.scheduleJob(jobDetail, trigger);
         } catch (SchedulerException e) {
