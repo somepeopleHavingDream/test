b70746a7e1a502a671a15404db8bbcc0e8a91222
yangxin <1393003255@qq.com>
2019-12-26 10:20:53 +0800

    feat: 测试HashMap的put方法

--- src/main/java/org/yangxin/test/map/HashMapTest.java
+++ src/main/java/org/yangxin/test/map/HashMapTest.java
@@ -1,8 +1,17 @@
 package org.yangxin.test.map;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author yangxin
  * 2019/12/25 11:58
  */
 public class HashMapTest {
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
+        String put1 = map.put("aa", "bb");
+        String put2 = map.put("aa", "cc");
+        System.out.println(put1 + " " + put2);
+    }
 }
