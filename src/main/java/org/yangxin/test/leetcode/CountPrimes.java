8237877a556f10b1fbec0ec2e6e4f2a6d5ef9c65
yangxin <1393003255@qq.com>
2020-01-06 17:48:50 +0800

    feat: CountPrimes

--- src/main/java/org/yangxin/test/leetcode/CountPrimes.java
+++ src/main/java/org/yangxin/test/leetcode/CountPrimes.java
@@ -1,5 +1,7 @@
 package org.yangxin.test.leetcode;
 
+import java.util.Arrays;
+
 /**
  * 统计素数
  *
@@ -8,27 +10,35 @@ package org.yangxin.test.leetcode;
  */
 public class CountPrimes {
     public static void main(String[] args) {
+        int result = countPrimes(4);
 //        int result = countPrimes(3);
-        int result = countPrimes(10);
+//        int result = countPrimes(0);
+//        int result = countPrimes(10);
         System.out.println(result);
     }
 
     public static int countPrimes(int n) {
-        int count = 0;
-        for (int i = 2; i < n; i++) {
-            boolean flag = false;
-            for (int j = 2; j < i; j++) {
-                if (i % j == 0) {
-                    break;
+        // isPrime[i]=true代表i是个素数
+        boolean[] isPrime = new boolean[n];
+
+        // 赋初值
+        Arrays.fill(isPrime, true);
+
+        // 考虑该数是否是个素数
+        for (int i = 2; i < Math.sqrt(n); i++) {
+            // 如果这个数是个素数，则所有它的倍数都不是素数
+            if (isPrime[i]) {
+                for (int j = i * 2; j < n; j += i) {
+                    isPrime[j] = false;
                 }
-
-                flag = true;
-            }
-
-            if (flag) {
-                count++;
             }
         }
+
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            count += isPrime[i] ? 1 : 0;
+        }
+
         return count;
     }
 }
