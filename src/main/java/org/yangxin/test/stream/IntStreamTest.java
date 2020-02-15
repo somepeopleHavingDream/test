4cfc189ff857170395b2c55ea5dfe48f3e392adf
yangxin <1393003255@qq.com>
2020-02-15 22:22:37 +0800

    feat: 增加IntStream的使用方法用例

---
+++ src/main/java/org/yangxin/test/stream/IntStreamTest.java
@@ -0,0 +1,19 @@
+package org.yangxin.test.stream;
+
+import java.util.stream.IntStream;
+
+/**
+ * @author yangxin
+ * 2020/02/15 21:20
+ */
+public class IntStreamTest {
+    public static void main(String[] args) {
+        IntStream.range(1, 10).forEach(System.out::println);
+
+        int sum = 0;
+        for (int i = 0; i < 10; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
