7ad663e357a398d633c980dd1a63f3a2bb9e4493
yangxin <1393003255@qq.com>
2020-03-05 18:01:53 +0800

    feat: 增加数字格式化类和小数格式化类的使用用例

---
+++ src/main/java/org/yangxin/test/format/DecimalFormatTest.java
@@ -0,0 +1,26 @@
+package org.yangxin.test.format;
+
+import java.text.DecimalFormat;
+
+/**
+ * 小数格式化
+ *
+ * @author yangxin
+ * 2020/03/05 17:49
+ */
+public class DecimalFormatTest {
+    public static void main(String[] args) {
+        double d1 = 123456.36987, d2 = 12.3698;
+        DecimalFormat decimalFormat = new DecimalFormat("0000.000");
+        System.out.println(decimalFormat.format(d1) + " " + decimalFormat.format(d2));
+
+        decimalFormat = new DecimalFormat("#");
+        System.out.println(decimalFormat.format(d1));
+
+        decimalFormat = new DecimalFormat(".####");
+        System.out.println(decimalFormat.format(d1));
+
+        decimalFormat = new DecimalFormat("0000,0000.0000");
+        System.out.println(decimalFormat.format(d1));
+    }
+}
