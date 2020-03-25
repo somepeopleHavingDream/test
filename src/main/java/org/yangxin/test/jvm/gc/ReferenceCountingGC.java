299294197b12d94f0d15bc0dd1850a7579347a39
yangxin <1393003255@qq.com>
2020-03-25 17:33:35 +0800

    feat: jvm虚拟机的gc算法是否是引用计数算法

--- src/main/java/org/yangxin/test/jvm/gc/ReferenceCountingGC.java
+++ src/main/java/org/yangxin/test/jvm/gc/ReferenceCountingGC.java
@@ -3,11 +3,13 @@ package org.yangxin.test.jvm.gc;
 /**
  * 垃圾回收算法-引用计数算法
  * testGC()方法执行后，objA和objB会不会被GC呢？
+ * -XX:+PrintGCDetails
  *
  * @author yangxin
  * 2019/11/12 17:49
  */
 public class ReferenceCountingGC {
+
     public Object instance = null;
 
     private static final int _1MB = 1024 * 1024;
