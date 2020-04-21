3cef0e234d13dd1592fbf891014869ada5e68ba8
yangxin <1393003255@qq.com>
2020-04-21 16:03:35 +0800

    feat: 动态对象年龄判定

---
+++ src/main/java/org/yangxin/test/jvm/memoryallocate/TenuringThresholdTest2.java
@@ -0,0 +1,27 @@
+package org.yangxin.test.jvm.memoryallocate;
+
+/**
+ * 动态对象年龄判定
+ *
+ * @author yangxin
+ * 2020/04/21 15:35
+ */
+public class TenuringThresholdTest2 {
+
+    private static final Integer _1MB = 1024 * 1024;
+
+    /**
+     * VM参数：-verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:SurvivorRatio=8 -XX:MaxTenuringThreshold=15 -XX:+PrintGCDetails -XX:+UseParNewGC
+     */
+    public static void main(String[] args) {
+        byte[] allocation1, allocation2, allocation3, allocation4;
+        allocation1 = new byte[_1MB / 4];
+
+        // allocation1 + allocation2大于survivor空间的一半
+        allocation2 = new byte[_1MB / 4];
+        allocation3 = new byte[4 * _1MB];
+        allocation4 = new byte[4 * _1MB];
+        allocation4 = null;
+        allocation4 = new byte[4 * _1MB];
+    }
+}
