cf043aa25b464876480195fd46fb700bc7c6bedf
yangxin <1393003255@qq.com>
2020-05-25 16:57:53 +0800

    feat: 循环标记retry的使用用例

---
+++ src/main/java/org/yangxin/test/thread/RetryTest.java
@@ -0,0 +1,24 @@
+package org.yangxin.test.thread;
+
+/**
+ * break retry
+ *
+ * @author yangxin
+ * 2020/05/25 16:49
+ */
+public class RetryTest {
+
+    public static void main(String[] args) {
+        int count = 0;
+        retry:
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 5; j++) {
+                count++;
+                if (count == 3) {
+                    break retry;
+                }
+                System.out.print(count + " ");
+            }
+        }
+    }
+}
