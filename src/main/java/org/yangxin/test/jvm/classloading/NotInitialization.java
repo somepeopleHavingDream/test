71d18af0919721053be3d6f0d355b3a32f7a172d
yangxin <1393003255@qq.com>
2020-06-18 18:04:00 +0800

    dev: 类加载时机，未完成

---
+++ src/main/java/org/yangxin/test/jvm/classloading/NotInitialization.java
@@ -0,0 +1,19 @@
+package org.yangxin.test.jvm.classloading;
+
+/**
+ * 非主动使用类字段演示
+ * -XX:+TraceClassLoading
+ *
+ * 被动演示类字段演示二：
+ * 通过数组定义来引用类，不会触发此类的初始化
+ *
+ * @author yangxin
+ * 2020/06/18 17:53
+ */
+public class NotInitialization {
+
+    public static void main(String[] args) {
+//        System.out.println(SubClass.value);
+        SuperClass[] superClasses = new SuperClass[10];
+    }
+}
