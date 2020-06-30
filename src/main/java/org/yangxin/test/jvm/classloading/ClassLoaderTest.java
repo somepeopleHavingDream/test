e49ce7b19ad96c2ef02bcb284d0022e430e016f3
yangxin <1393003255@qq.com>
2020-06-30 14:53:40 +0800

    feat: 获取根类加载器所加载的核心类库，并会看到本机安装的Java环境变量指定的JDK中提供的核心jar包路径

--- src/main/java/org/yangxin/test/jvm/classloading/ClassLoaderTest.java
+++ src/main/java/org/yangxin/test/jvm/classloading/ClassLoaderTest.java
@@ -1,7 +1,8 @@
 package org.yangxin.test.jvm.classloading;
 
-import java.io.IOException;
-import java.io.InputStream;
+import sun.misc.Launcher;
+
+import java.net.URL;
 
 /**
  * 不同的类加载器对instanceof关键字运算结果的影响
@@ -13,30 +14,42 @@ import java.io.InputStream;
 public class ClassLoaderTest {
 
     public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
-        ClassLoader myLoader = new ClassLoader() {
+        printJDKJar();
+//        ClassLoader myLoader = new ClassLoader() {
+//
+//            @SuppressWarnings("ResultOfMethodCallIgnored")
+//            @Override
+//            public Class<?> loadClass(String name) throws ClassNotFoundException {
+//                String fileName = name.substring(name.lastIndexOf(".") + 1) + ".class";
+//                System.out.println("fileName: " + fileName);
+//                InputStream inputStream = getClass().getResourceAsStream(fileName);
+//                if (inputStream == null) {
+//                    return super.loadClass(name);
+//                }
+//
+//                try {
+//                    byte[] b = new byte[inputStream.available()];
+//                    inputStream.read(b);
+//                    return defineClass(name, b, 0, b.length);
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                    throw new ClassNotFoundException(name);
+//                }
+//            }
+//        };
+//
+//        Object object = myLoader.loadClass("org.yangxin.test.jvm.classloading.ClassLoaderTest").newInstance();
+//        System.out.println(object.getClass());
+//        System.out.println(object instanceof org.yangxin.test.jvm.classloading.ClassLoaderTest);
+    }
 
-            @Override
-            public Class<?> loadClass(String name) throws ClassNotFoundException {
-                String fileName = name.substring(name.lastIndexOf(".") + 1) + ".class";
-                System.out.println("fileName: " + fileName);
-                InputStream inputStream = getClass().getResourceAsStream(fileName);
-                if (inputStream == null) {
-                    return super.loadClass(name);
-                }
-
-                try {
-                    byte[] b = new byte[inputStream.available()];
-                    inputStream.read(b);
-                    return defineClass(name, b, 0, b.length);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    throw new ClassNotFoundException(name);
-                }
-            }
-        };
-
-        Object object = myLoader.loadClass("org.yangxin.test.jvm.classloading.ClassLoaderTest").newInstance();
-        System.out.println(object.getClass());
-        System.out.println(object instanceof org.yangxin.test.jvm.classloading.ClassLoaderTest);
+    /**
+     * 获取根类加载器所加载的核心类库，并会看到本机安装的Java环境变量指定的JDK中提供的核心jar包路径
+     */
+    private static void printJDKJar() {
+        URL[] urLs = Launcher.getBootstrapClassPath().getURLs();
+        for (URL urL : urLs) {
+            System.out.println(urL.toExternalForm());
+        }
     }
 }
