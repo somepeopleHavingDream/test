d3358575e40bca28cf7ab54096112f6d53ae5ca6
yangxin <1393003255@qq.com>
2020-07-01 11:26:51 +0800

    feat: 条形码底部加文字

---
+++ src/main/java/org/yangxin/test/zxing/CreateBarcodeWithWords.java
@@ -0,0 +1,80 @@
+package org.yangxin.test.zxing;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.oned.Code128Writer;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+
+/**
+ * 生成条码号，底部带文字
+ *
+ * @author yangxin
+ * 2020/07/01 09:05
+ */
+public class CreateBarcodeWithWords {
+
+    public static void main(String[] args) {
+        Code128Writer code128Writer = new Code128Writer();
+        try {
+            BitMatrix bitMatrix = code128Writer.encode("uzi is god forever!",
+                    BarcodeFormat.CODE_128,
+                    264,
+                    48,
+                    new HashMap<>());
+
+            // 使用BufferedImage勾画Barcode
+            int width = bitMatrix.getWidth();
+            int height = bitMatrix.getHeight();
+            BufferedImage bufferedImage = new BufferedImage(width,
+                    height,
+                    BufferedImage.TYPE_INT_RGB);
+            Graphics2D graphics2D = bufferedImage.createGraphics();
+            graphics2D.setColor(Color.WHITE);
+            graphics2D.fillRect(0, 0, width, height);
+
+            // 使用比特矩阵画并保存图像
+            graphics2D.setColor(Color.BLACK);
+            for (int i = 0; i < width; i++) {
+                for (int j = 0; j < height; j++) {
+                    if (bitMatrix.get(i, j)) {
+                        graphics2D.fillRect(i, j, 1, 1);
+                    }
+                }
+            }
+            bufferedImage.flush();
+
+            // 新的图片
+            BufferedImage outImage = new BufferedImage(270, 60, BufferedImage.TYPE_INT_RGB);
+            Graphics2D outImageGraphics = outImage.createGraphics();
+            outImageGraphics.setColor(Color.WHITE);
+            outImageGraphics.fillRect(0, 0, 270, 60);
+            outImageGraphics.setColor(Color.BLACK);
+
+            // 画二维码到新的面板
+            outImageGraphics.drawImage(bufferedImage, 5, 5, 264, 48, null);
+
+            // 画文字到新的面板
+            // 字体、字型、字号
+            Font font = new Font("微软雅黑", Font.PLAIN, 8);
+            outImageGraphics.setFont(font);
+
+            // 文字长度
+            int strWidth = graphics2D.getFontMetrics().stringWidth("uzi is god forever!");
+            // 总长度减去文字长度的一般（居中显示）
+            int wordStartX = (300 - strWidth) / 2;
+            outImageGraphics.drawString("20154206110", wordStartX, 60);
+            outImage.flush();
+
+            ImageIO.write(outImage, "PNG", new File("image/barcodeWithWords.png"));
+        } catch (WriterException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/main/java/org/yangxin/test/zxing/CreateQRCodeWithWords.java
+++ src/main/java/org/yangxin/test/zxing/CreateQRCodeWithWords.java
@@ -20,17 +20,18 @@ import java.util.Map;
  * @author yangxin
  * 2020/06/30 16:30
  */
-public class QrCodeCreateUtil {
+public class CreateQRCodeWithWords {
 
     /**
      * 生成包含字符串信息的二维码图片
      *
      * @param outputStream 文件输出流路径
-     * @param content 二维码携带信息
-     * @param qrCodeSize 二维码图片大小
-     * @param imageFormat 二维码格式
+     * @param content      二维码携带信息
+     * @param qrCodeSize   二维码图片大小
+     * @param imageFormat  二维码格式
      */
-    public static boolean createQrCode(OutputStream outputStream, String content, int qrCodeSize, String imageFormat) {
+    @SuppressWarnings("SuspiciousNameCombination")
+    public static void createQrCode(OutputStream outputStream, String content, int qrCodeSize, String imageFormat) {
         // 设置二维码纠错级别Map
         Map<EncodeHintType, ErrorCorrectionLevel> hintMap = new HashMap<>();
         // 纠错级别
@@ -43,9 +44,11 @@ public class QrCodeCreateUtil {
 
             // 使用BufferedImage勾画QRCode（matrixWidth是行二维码像素点）
             int matrixWidth = bitMatrix.getWidth();
-            BufferedImage bufferedImage = new BufferedImage(matrixWidth - 200, matrixWidth - 200, BufferedImage.TYPE_INT_RGB);
-            bufferedImage.createGraphics();
-            Graphics2D graphics2D = (Graphics2D) bufferedImage.getGraphics();
+            BufferedImage bufferedImage = new BufferedImage(matrixWidth - 200,
+                    matrixWidth - 200,
+                    BufferedImage.TYPE_INT_RGB);
+            Graphics2D graphics2D = bufferedImage.createGraphics();
+//            Graphics2D graphics2D = (Graphics2D) bufferedImage.getGraphics();
             graphics2D.setColor(Color.WHITE);
             graphics2D.fillRect(0, 0, matrixWidth, matrixWidth);
 
@@ -60,42 +63,50 @@ public class QrCodeCreateUtil {
             }
             bufferedImage.flush();
 
-            BufferedImage outImage = new BufferedImage(800, 800, BufferedImage.TYPE_INT_ARGB);
+//            ImageIO.write(bufferedImage, imageFormat, outputStream);
+
+            BufferedImage outImage = new BufferedImage(800, 800, BufferedImage.TYPE_INT_RGB);
+//            BufferedImage outImage = new BufferedImage(800, 800, BufferedImage.TYPE_INT_ARGB);
             Graphics2D outImageGraphics = outImage.createGraphics();
             outImageGraphics.setColor(Color.WHITE);
             outImageGraphics.fillRect(0, 0, 800, 800);
             outImageGraphics.setColor(Color.BLACK);
 
             // 画二维码到新的面板
-            outImageGraphics.drawImage(bufferedImage, 100, 100, bufferedImage.getWidth(), bufferedImage.getHeight(), null);
-
+            outImageGraphics.drawImage(bufferedImage,
+                    100,
+                    100,
+                    bufferedImage.getWidth(),
+                    bufferedImage.getHeight(), null);
+//
             // 画文字到新的面板
             // 字体、字型、字号
             Font font = new Font("宋体", Font.PLAIN, 50);
             outImageGraphics.setFont(font);
 
             // 文字长度
+//            int strWidth = outImageGraphics.getFontMetrics().stringWidth("B0000001");
             int strWidth = graphics2D.getFontMetrics().stringWidth("B0000001");
             // 总长度减去文字长度的一般（居中显示）
-            int wordStartX = (700 - strWidth) / 2;
+            int wordStartX = (600 - strWidth) / 2;
+//            int wordStartX = (700 - strWidth) / 2;
             // 画文字
             outImageGraphics.drawString(content, wordStartX, 750);
             outImage.flush();
-
-            return ImageIO.write(outImage, imageFormat, outputStream);
+//
+            ImageIO.write(outImage, imageFormat, outputStream);
         } catch (WriterException | IOException e) {
             e.printStackTrace();
         }
 
-        return false;
     }
 
-    public static String getWillNo(String no) {
-        String s = no.substring(0, 6);
-        int willNo = Integer.parseInt(no.substring(6, 8));
-
-        return s + willNo + 1;
-    }
+//    public static String getWillNo(String no) {
+//        String s = no.substring(0, 6);
+//        int willNo = Integer.parseInt(no.substring(6, 8));
+//
+//        return s + willNo + 1;
+//    }
 
     public static void main(String[] args) throws FileNotFoundException {
         System.out.println("W0000021");
