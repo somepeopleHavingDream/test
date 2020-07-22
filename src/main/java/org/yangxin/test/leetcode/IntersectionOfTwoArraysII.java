cf1e827ce7ce19335958a3708aecc502f6384f60
yangxin <1393003255@qq.com>
2020-07-22 13:53:27 +0800

    feat: Intersection of Two Arrays II

--- src/main/java/org/yangxin/test/leetcode/IntersectionOfTwoArraysII.java
+++ src/main/java/org/yangxin/test/leetcode/IntersectionOfTwoArraysII.java
@@ -1,43 +1,45 @@
 package org.yangxin.test.leetcode;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
 
 public class IntersectionOfTwoArraysII {
 
     private static int[] intersect(int[] nums1, int[] nums2) {
-        // 先将两个数组升序
+        // 先将数组升序，如：num1: 1, 2, 3 num2: 1, 1
         Arrays.sort(nums1);
         Arrays.sort(nums2);
-        System.out.println(Arrays.toString(nums1));
-        System.out.println(Arrays.toString(nums2));
 
-        List<Integer> list = new LinkedList<>();
+        List<Integer> resultList = new ArrayList<>();
         int i = 0, j = 0;
         while (i < nums1.length && j < nums2.length) {
-            while (i < nums1.length && nums1[i] < nums2[j]) {i++;}
-            while (j < nums2.length && nums2[j] < nums1[i]) {j++;}
-
-            if (j < nums2.length && nums1[i] == nums2[j]) {
-                list.add(nums1[i]);
+            // 如果两个数组当前的位置的元素值相同，则进入线性表
+            if (nums1[i] == nums2[j]) {
+                resultList.add(nums1[i]);
                 i++;
+                j++;
+            } else if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
                 j++;
             }
         }
 
-        return list.stream().mapToInt(Integer::intValue).toArray();
+        int[] results = new int[resultList.size()];
+        for (int k = 0; k < results.length; k++) {
+            results[k] = resultList.get(k);
+        }
+        return results;
     }
 
     public static void main(String[] args) {
-        int[] nums1 = {7, 2, 2, 4, 7, 0, 3, 4, 5};
-//        int[] nums1 = {4, 9, 5};
-//        int[] nums1 = {1, 2};
 //        int[] nums1 = {1, 2, 2, 1};
-        int[] nums2 = {3, 9, 8, 6, 1, 9};
-//        int[] nums2 = {9, 4, 9, 8, 4};
+        int[] nums1 = {3, 1, 2};
 //        int[] nums2 = {2, 2};
-//        int[] nums2 = {1, 1};
+//        int[] nums1 = {4, 9, 5};
+//        int[] nums2 = {9, 4, 9, 8, 4};
+        int[] nums2 = {1,  1};
 //        int[] nums2 = {2};
         System.out.println(Arrays.toString(intersect(nums1, nums2)));
     }
