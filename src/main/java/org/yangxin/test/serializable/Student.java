97b580773f07da05aabd5153f7ff47d78b3de004
yangxin <1393003255@qq.com>
2020-07-29 16:50:35 +0800

    feat: 观察序列化源码

--- src/main/java/org/yangxin/test/serializable/Student.java
+++ src/main/java/org/yangxin/test/serializable/Student.java
@@ -5,7 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.io.Serializable;
+import java.io.*;
 
 /**
  * @author yangxin
@@ -15,10 +15,20 @@ import java.io.Serializable;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+//public class Student {
 public class Student implements Serializable {
 
     private static final long serialVersionUID = -363987537630696589L;
 
     private String username;
     private Integer age;
+
+    public static void main(String[] args) throws IOException {
+        Student alice = new Student("Alice", 13);
+        Student bob = new Student("Bob", 15);
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream("stu.dat"));
+        objectOutputStream.writeObject(alice);
+        objectOutputStream.writeObject(bob);
+        objectOutputStream.close();
+    }
 }
