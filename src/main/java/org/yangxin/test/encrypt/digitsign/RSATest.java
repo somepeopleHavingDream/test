7d99510323c3b1d81aa7fd3bd89e1e186c3e7424
yangxin <1393003255@qq.com>
2020-10-08 18:06:41 +0800

    feat: 数字签名算法RSA

---
+++ src/main/java/org/yangxin/test/encrypt/digitsign/RSATest.java
@@ -0,0 +1,56 @@
+package org.yangxin.test.encrypt.digitsign;
+
+import org.apache.commons.codec.binary.Hex;
+
+import java.security.*;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+
+/**
+ * @author yangxin
+ * 2020/10/08 17:56
+ */
+public class RSATest {
+
+    private static final String SRC = "imooc security rsa";
+
+    public static void main(String[] args) {
+        jdkRSA();
+    }
+
+    private static void jdkRSA() {
+        try {
+            // 1. 初始化密钥
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPairGenerator.initialize(512);
+            KeyPair keyPair = keyPairGenerator.generateKeyPair();
+            RSAPublicKey rsaPublicKey = (RSAPublicKey) keyPair.getPublic();
+            RSAPrivateKey rsaPrivateKey = (RSAPrivateKey) keyPair.getPrivate();
+
+            // 2. 执行签名
+            PKCS8EncodedKeySpec pkcs8EncodedKeySpec = new PKCS8EncodedKeySpec(rsaPrivateKey.getEncoded());
+            KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            PrivateKey privateKey = keyFactory.generatePrivate(pkcs8EncodedKeySpec);
+            Signature signature = Signature.getInstance("MD5withRSA");
+            signature.initSign(privateKey);
+            signature.update(SRC.getBytes());
+            byte[] result = signature.sign();
+            System.out.println("jdk rsa sign: " + Hex.encodeHexString(result));
+
+            // 3. 验证签名
+            X509EncodedKeySpec x509EncodedKeySpec = new X509EncodedKeySpec(rsaPublicKey.getEncoded());
+            keyFactory = KeyFactory.getInstance("RSA");
+            PublicKey publicKey = keyFactory.generatePublic(x509EncodedKeySpec);
+            signature = Signature.getInstance("MD5withRSA");
+            signature.initVerify(publicKey);
+            signature.update(SRC.getBytes());
+            boolean bool = signature.verify(result);
+            System.out.println("jdk rsa verify: " + bool);
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException | InvalidKeyException | SignatureException e) {
+            e.printStackTrace();
+        }
+    }
+}
