8ae1ea56c9cf6121efdd1e941eede1bd3af73dce
yangxin <1393003255@qq.com>
2020-10-08 18:19:47 +0800

    feat: 数字签名算法DSA

---
+++ src/main/java/org/yangxin/test/encrypt/digitsign/DSATest.java
@@ -0,0 +1,56 @@
+package org.yangxin.test.encrypt.digitsign;
+
+import org.apache.commons.codec.binary.Hex;
+
+import java.security.*;
+import java.security.interfaces.DSAPrivateKey;
+import java.security.interfaces.DSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+
+/**
+ * @author yangxin
+ * 2020/10/08 18:10
+ */
+public class DSATest {
+
+    private static final String SRC = "imooc security dsa";
+
+    public static void main(String[] args) {
+        jdkDSA();
+    }
+
+    private static void jdkDSA() {
+        try {
+            // 1. 初始化密钥
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("DSA");
+            keyPairGenerator.initialize(512);
+            KeyPair keyPair = keyPairGenerator.generateKeyPair();
+            DSAPublicKey dsaPublicKey = (DSAPublicKey) keyPair.getPublic();
+            DSAPrivateKey dsaPrivateKey = (DSAPrivateKey) keyPair.getPrivate();
+
+            // 2. 执行签名
+            PKCS8EncodedKeySpec pkcs8EncodedKeySpec = new PKCS8EncodedKeySpec(dsaPrivateKey.getEncoded());
+            KeyFactory keyFactory = KeyFactory.getInstance("DSA");
+            PrivateKey privateKey = keyFactory.generatePrivate(pkcs8EncodedKeySpec);
+            Signature signature = Signature.getInstance("SHA1withDSA");
+            signature.initSign(privateKey);
+            signature.update(SRC.getBytes());
+            byte[] result = signature.sign();
+            System.out.println("jdk dsa sign: " + Hex.encodeHexString(result));
+
+            // 3. 验证签名
+            X509EncodedKeySpec x509EncodedKeySpec = new X509EncodedKeySpec(dsaPublicKey.getEncoded());
+            keyFactory = KeyFactory.getInstance("DSA");
+            PublicKey publicKey = keyFactory.generatePublic(x509EncodedKeySpec);
+            signature = Signature.getInstance("SHA1withDSA");
+            signature.initVerify(publicKey);
+            signature.update(SRC.getBytes());
+            boolean bool = signature.verify(result);
+            System.out.println("jdk dsa verify: " + bool);
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException | InvalidKeyException | SignatureException e) {
+            e.printStackTrace();
+        }
+    }
+}
