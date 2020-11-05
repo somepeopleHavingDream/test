08d5ffa4776e42cabe884da3b9edb16ae069ce95
yangxin <1393003255@qq.com>
2020-11-05 14:44:35 +0800

    feat: 增加des对称加密算法的使用用例。

--- src/main/java/org/yangxin/test/encrypt/symmetric/DESTest.java
+++ src/main/java/org/yangxin/test/encrypt/symmetric/DESTest.java
@@ -5,16 +5,14 @@ import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 import javax.crypto.*;
 import javax.crypto.spec.DESKeySpec;
-import java.security.InvalidKeyException;
-import java.security.Key;
-import java.security.NoSuchAlgorithmException;
-import java.security.Security;
+import java.security.*;
 import java.security.spec.InvalidKeySpecException;
 
 /**
  * @author yangxin
  * 2020/09/29 17:02
  */
+@SuppressWarnings("DuplicatedCode")
 public class DESTest {
 
     private static final String src = "imooc security des";
@@ -78,8 +76,119 @@ public class DESTest {
         }
     }
 
+    /**
+     * 解密方法
+     */
+    public static String decrypt(String skey, String encryptedString)
+            throws IllegalBlockSizeException, BadPaddingException, InvalidKeyException,
+            NoSuchAlgorithmException,
+            NoSuchPaddingException, InvalidKeySpecException {
+        byte[] encryptedData = hexToBytes(encryptedString.toCharArray());
+        byte[] rawKeyData = hexToBytes(skey.toCharArray());
+        // DES算法要求有一个可信任的随机数源
+        SecureRandom sr = new SecureRandom();
+        // 从原始密匙数据创建一个DESKeySpec对象
+        DESKeySpec dks = new DESKeySpec(rawKeyData);
+        // 创建一个密匙工厂，然后用它把DESKeySpec对象转换成一个SecretKey对象
+        SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
+        SecretKey key = keyFactory.generateSecret(dks);
+        // Cipher对象实际完成解密操作
+        Cipher cipher = Cipher.getInstance("DES");
+        // 用密匙初始化Cipher对象
+        cipher.init(Cipher.DECRYPT_MODE, key, sr);
+        // 正式执行解密操作
+        byte[] decryptedData = cipher.doFinal(encryptedData);
+        return new String(decryptedData);
+    }
+
+    private static byte[] hexToBytes(char[] hex) {
+        int length = hex.length / 2;
+        byte[] raw = new byte[length];
+        for (int i = 0; i < length; i++) {
+            int high = Character.digit(hex[i * 2], 16);
+            int low = Character.digit(hex[i * 2 + 1], 16);
+            int value = (high << 4) | low;
+            if (value > 127)
+                value -= 256;
+            raw[i] = (byte) value;
+        }
+        return raw;
+    }
+
+    /**
+     * 加密方法
+     */
+    public static String encrypt(String skey, String str)
+            throws InvalidKeyException, NoSuchAlgorithmException,
+            IllegalBlockSizeException, BadPaddingException,
+            NoSuchPaddingException, InvalidKeySpecException {
+        byte[] rawKeyData = hexToBytes(skey.toCharArray());
+
+        // DES算法要求有一个可信任的随机数源
+        SecureRandom sr = new SecureRandom();
+
+        // 从原始密匙数据创建一个DESKeySpec对象
+        DESKeySpec dks = new DESKeySpec(rawKeyData);
+
+        // 创建一个密匙工厂，然后用它把DESKeySpec转换成一个SecretKey对象
+        SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
+        SecretKey key = keyFactory.generateSecret(dks);
+
+        // Cipher对象实际完成加密操作
+        Cipher cipher = Cipher.getInstance("DES");
+        // 用密匙初始化Cipher对象
+        cipher.init(Cipher.ENCRYPT_MODE, key, sr);
+
+        // 现在，获取数据并加密
+        byte[] data = str.getBytes();
+
+        // 正式执行加密操作
+        byte[] encryptedData = cipher.doFinal(data);
+
+        return byteArrayToHexString(encryptedData);
+    }
+
+    private static String byteArrayToHexString(byte[] b) {
+        StringBuilder sb = new StringBuilder(b.length * 2);
+
+        for (byte value : b) {
+            int v = value & 0xff;
+
+            if (v < 16) {
+                sb.append('0');
+            }
+
+            sb.append(Integer.toHexString(v));
+        }
+
+        return sb.toString().toUpperCase();
+    }
+
     public static void main(String[] args) {
-        jdkDES();
-        bcDES();
+//        jdkDES();
+//        bcDES();
+
+//        String rdid = "test001";
+        String key = "a1061dfaf0192b7f";
+//        String key = "203961a7a5f21194";
+//        String key = "cd4a26851bfca90c";
+        String encryptStr = "B98749E406784B6AF452FF958073BC686474DDEB321EC33E6D9862DD9E34FD8A";
+//        String encryptStr = "84E37E7DAEA51CDB250FA9D0B8941847FFAA46A77CD9AC35F3BF5B8CDE1AD32C";
+//        String encryptStr = "A132E44C2A3AFDA2";
+
+//        System.out.println("原文：" + rdid);
+        System.out.println("加密：" + encryptStr);
+
+        String Str1 = null;
+        String Str2 = null;
+        try {
+//            Str1 = encrypt(key, rdid);
+            Str2 = decrypt(key, encryptStr);
+
+        } catch (Exception e) {
+            System.out.println("解密失败");
+        }
+//        System.out.println("加密结果：" + Str1);
+        System.out.println("解密结果：" + Str2);
     }
 }
