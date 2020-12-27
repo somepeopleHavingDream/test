84196b449d1556b67c1cd98accf5afdb819fb942
yangxin <1393003255@qq.com>
2020-12-28 01:10:42 +0800

    feat: 自定义线程池

---
+++ src/main/java/org/yangxin/test/thread/ThreadTest.java
@@ -0,0 +1,38 @@
+package org.yangxin.test.thread;
+
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author yangxin
+ * 12/27/20 11:13 PM
+ */
+public class ThreadTest implements Runnable {
+
+    public static void main(String[] args) throws InterruptedException {
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(4,
+                4,
+                0,
+                TimeUnit.MILLISECONDS,
+                new LinkedBlockingQueue<>());
+        for (int i = 0; i < 5; i++) {
+            threadPoolExecutor.execute(new ThreadTest());
+        }
+
+        TimeUnit.SECONDS.sleep(5);
+        System.out.println("threadPoolExecutor: " + threadPoolExecutor);
+        System.out.println("activeCount: " + threadPoolExecutor.getActiveCount());
+        System.out.println("completedTaskCount: " + threadPoolExecutor.getCompletedTaskCount());
+        System.out.println("corePoolSize: " + threadPoolExecutor.getCorePoolSize());
+        System.out.println("largestPoolSize: " + threadPoolExecutor.getLargestPoolSize());
+        System.out.println("maximumPoolSize: " + threadPoolExecutor.getMaximumPoolSize());
+        System.out.println("poolSize: " + threadPoolExecutor.getPoolSize());
+        System.out.println("taskCount: " + threadPoolExecutor.getTaskCount());
+    }
+
+    @Override
+    public void run() {
+        System.out.println("ok.");
+    }
+}
