6657659a85f8ca2b36c38f8b03f298cfe52c862c
yangxin <1393003255@qq.com>
2021-01-15 00:32:26 +0800

    feat: 增加LockSupport的使用用例

--- src/main/java/org/yangxin/test/retry/RetryTest.java
+++ src/main/java/org/yangxin/test/retry/RetryTest.java
@@ -9,17 +9,52 @@ package org.yangxin.test.retry;
 public class RetryTest {
 
     public static void main(String[] args) {
-        int count = 0;
-        retry:
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < 5; j++) {
-                count++;
-                if (count == 3) {
+//        int count = 0;
+//
+//        retry:
+////        System.out.println("retry..");
+//        for (int i = 0; i < 2; i++) {
+//            for (int j = 0; j < 5; j++) {
+//                count++;
+//                if (count == 3) {
 //                    continue retry;
-                    break retry;
+////                    break retry;
+//                }
+//                System.out.print(count + " ");
+//            }
+//        }
+
+        testContinue();
+        testBreak();
+    }
+
+    public static void testContinue() {
+        retry:
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print(j + " ");
+
+                if (j == 3) {
+                    continue retry;
                 }
-                System.out.print(count + " ");
             }
         }
+
+        System.out.println(" >>> ok");
+    }
+
+    public static void testBreak() {
+        retry:
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print(j + " ");
+
+                if (j == 3) {
+                    break retry;
+                }
+            }
+        }
+
+        System.out.println(" >>> ok");
     }
 }
