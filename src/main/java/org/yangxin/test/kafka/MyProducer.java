686ebebf8c7c0e8af53b2e0c047e53a7223f2000
yangxin <1393003255@qq.com>
2021-01-23 17:30:57 +0800

    comment: 对MyProducer类的有关方法做了一些注释。

--- src/main/java/org/yangxin/test/kafka/MyProducer.java
+++ src/main/java/org/yangxin/test/kafka/MyProducer.java
@@ -14,7 +14,6 @@ import java.util.concurrent.ExecutionException;
  * 2020/08/24 10:57
  */
 @SuppressWarnings("unused")
-//@Slf4j
 public class MyProducer {
 
     private static final KafkaProducer<String, String> PRODUCER;
@@ -22,7 +21,6 @@ public class MyProducer {
     static {
         Properties properties = new Properties();
         properties.put("bootstrap.servers", "192.168.3.3:9092");
-//        properties.put("bootstrap.servers", "127.0.0.1:9092");
         properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         properties.put("partitioner.class", "org.yangxin.test.kafka.CustomPartitioner");
@@ -42,17 +40,8 @@ public class MyProducer {
                 "name", "blocked");
         RecordMetadata recordMetadata = PRODUCER.send(record).get();
 
-//        System.out.println("topic: " + recordMetadata.topic());
-//        System.out.println("partition: " + recordMetadata.partition());
-//        System.out.println("offset: " + recordMetadata.offset());
-//        System.out.println("topic: [%s], partition: [%s], offset: [%]",
-//                recordMetadata.topic(), recordMetadata.partition(), recordMetadata.offset());
         System.out.printf("topic: [%s], partition: [%s], offset: [%s]%n",
                 recordMetadata.topic(), recordMetadata.partition(), recordMetadata.offset());
-
-//        log.info("recordMetadata: [{}]", recordMetadata);
-//        log.info("topic: [{}], partition: [{}], offset: [{}]",
-//                recordMetadata.topic(), recordMetadata.partition(), recordMetadata.offset());
 
         PRODUCER.close();
     }
@@ -94,20 +83,12 @@ public class MyProducer {
                 return;
             }
 
-//            System.out.println("topic: " + recordMetadata.topic());
-//            System.out.println("partition: " + recordMetadata.partition());
-//            System.out.println("offset: " + recordMetadata.offset());
             System.out.printf("topic: [%s], partition: [%s], offset: [%s]%n",
                     recordMetadata.topic(), recordMetadata.partition(), recordMetadata.offset());
-
-//            log.info("topic: [{}], partition: [{}], offset: [{}]",
-//                    recordMetadata.topic(), recordMetadata.partition(), recordMetadata.offset());
         }
     }
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         sendMessageCallback();
-//        sendMessageSync();
-//        sendMessageForgetResult();
     }
 }
