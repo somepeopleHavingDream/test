b2b68913b57aa19a2a6ec85ef51d46b97b8ffe42
yangx@mashang.cn <yangx@mashang.cn>
2021-04-20 18:21:49 +0800

    feat: 增加时间戳的相关使用

--- src/main/java/org/yangxin/test/datetime/TimeStampTest.java
+++ src/main/java/org/yangxin/test/datetime/TimeStampTest.java
@@ -1,5 +1,9 @@
 package org.yangxin.test.datetime;
 
+import java.text.DateFormat;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 /**
@@ -11,6 +15,26 @@ import java.util.Date;
 public class TimeStampTest {
 
     public static void main(String[] args) {
+//        getTimeStamp();
+
+        String timestamp = "1618780339154";
+        System.out.println(timestamp);
+        long currentTimeMillis = System.currentTimeMillis();
+        System.out.println(currentTimeMillis);
+
+        Date date = new Date(Long.parseLong(timestamp));
+        System.out.println(date);
+
+        LocalDateTime localDateTime = LocalDateTime.ofEpochSecond(Long.parseLong(timestamp) / 1000,
+                0, ZoneOffset.ofHours(8));
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyMMddHHmmss");
+        System.out.println(localDateTime.getYear());
+        System.out.println(localDateTime.getMonth());
+        System.out.println(localDateTime.getDayOfMonth());
+        System.out.println(dateTimeFormatter.format(localDateTime));
+    }
+
+    private static void getTimeStamp() {
         Date date = new Date();
         System.out.println(date.getTime());
         System.out.println(System.currentTimeMillis());
