17c8de931997c84a7cd8e69df5c9513448367173
yangx@mashang.cn <yangx@mashang.cn>
2021-06-17 18:21:20 +0800

    feat: 增加整型时间戳转换为时间字符串的案例

--- src/main/java/org/yangxin/test/datetime/LongTest.java
+++ src/main/java/org/yangxin/test/datetime/LongTest.java
@@ -1,6 +1,10 @@
 package org.yangxin.test.datetime;
 
 import java.sql.Timestamp;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 
 /**
  * @author yangxin
@@ -9,7 +13,8 @@ import java.sql.Timestamp;
 public class LongTest {
 
     public static void main(String[] args) {
-        test1();
+//        test1();
+        test2();
     }
 
     /**
@@ -17,6 +22,20 @@ public class LongTest {
      */
     public static void test1() {
         long currentTimeMillis = System.currentTimeMillis();
-        System.out.println(new Timestamp((currentTimeMillis)));
+        System.out.println(new Timestamp(currentTimeMillis));
+    }
+
+    /**
+     * 整型时间戳转日期字符串
+     */
+    public static void test2() {
+        long timestamp = 1623035797428L;
+        LocalDateTime localDateTime = LocalDateTime.ofEpochSecond(timestamp / 1000,
+                0, ZoneOffset.ofHours(8));
+
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+        System.out.println(dateTimeFormatter.format(localDateTime));
+
+//        System.out.println(Instant.ofEpochSecond(timestamp).atZone(ZoneOffset.ofHours(8)).toLocalDateTime());
     }
 }
