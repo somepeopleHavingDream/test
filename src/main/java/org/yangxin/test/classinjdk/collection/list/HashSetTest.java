e0d0d711a214e4f9acc59b54ae5298eccf3178a6
yangxin <1393003255@qq.com>
2021-08-27 18:18:00 +0800

    feat: retainAll的使用案例

---
+++ src/main/java/org/yangxin/test/classinjdk/collection/list/HashSetTest.java
@@ -0,0 +1,27 @@
+package org.yangxin.test.classinjdk.collection.list;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * @author yangxin
+ * 2021/8/27 17:50
+ */
+@SuppressWarnings({"AlibabaRemoveCommentedCode", "CommentedOutCode"})
+public class HashSetTest {
+
+    public static void main(String[] args) {
+        test1();
+    }
+
+    private static void test1() {
+        Set<String> set1 = Stream.of("a", "b", "c").collect(Collectors.toSet());
+        Set<String> set2 = Stream.of("a", "b").collect(Collectors.toSet());
+
+//        set1.retainAll(set2);
+//        set1.removeAll(set2);
+        set2.removeAll(set1);
+        System.out.println(set2);
+    }
+}
