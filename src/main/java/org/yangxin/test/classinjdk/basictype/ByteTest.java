14a30dfcfaa541c65cb567f047349bb97f3e860c
yangxin <1393003255@qq.com>
2021-09-14 19:10:29 +0800

    feat: XmlMapper的一些用例

--- src/main/java/org/yangxin/test/classinjdk/basictype/ByteTest.java
+++ src/main/java/org/yangxin/test/classinjdk/basictype/ByteTest.java
@@ -10,10 +10,19 @@ public class ByteTest {
 
     public static void main(String[] args) {
 //        test1();
+        test2();
     }
 
+    /**
+     * 字节数组转字符串
+     */
     private static void test1() {
         byte[] header = new byte[]{7, 7, 7, 7, 7, 7, 7, 7};
         System.out.println(Arrays.toString(header));
+    }
+
+    private static void test2() {
+        byte a = 1;
+        System.out.println(a);
     }
 }
--- src/main/java/org/yangxin/test/json/jackson/XmlMapperTest.java
+++ src/main/java/org/yangxin/test/json/jackson/XmlMapperTest.java
@@ -1,6 +1,7 @@
 package org.yangxin.test.json.jackson;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
 import lombok.AllArgsConstructor;
@@ -14,26 +15,35 @@ import java.util.List;
  * @author yangxin
  * 2021/4/16 14:50
  */
-@SuppressWarnings({"AlibabaRemoveCommentedCode", "CommentedOutCode"})
+@SuppressWarnings({"AlibabaRemoveCommentedCode"})
 public class XmlMapperTest {
 
+    private static final XmlMapper MAPPER = new XmlMapper();
+
     public static void main(String[] args) throws JsonProcessingException {
-        XmlMapper xmlMapper = new XmlMapper();
+//        test1();
+        test2();
+    }
 
-//        String s = xmlMapper.writeValueAsString(new SimpleBean());
-//        System.out.println(s);
+    private static void test2() throws JsonProcessingException {
+        JsonNode node = MAPPER.readTree("<EStudentCertificateDTO><code>1</code><msg>操作成功</msg><cardId>TRTW54345343456784</cardId><terminalId>869523050457556</terminalId><userId>20006972939</userId><schoolId>57063300</schoolId></EStudentCertificateDTO>");
+        JsonNode code = node.get("code");
 
-//        SimpleBean value = xmlMapper.readValue("<SimpleBean><x>1</x><y>2</y></SimpleBean>", SimpleBean.class);
-//        System.out.println(value);
+        // asText()会进行强转，如果字段值不是String类型数据，会将其转成String基本类型的数据（如果是复杂类型，返回空字符串）
+        System.out.println(code.asText());
+        System.out.println(code);
+        // textValue()只针对String类型数据，所以如果字段值是非String类型数据，则返回null
+        System.out.println(code.textValue());
+        System.out.println(code.toPrettyString());
+    }
 
-//        Person person = new Person();
-//        person.setFirstName("Alice");
-//        person.setLastName("Bob");
-//        person.setPhoneNumbers(Arrays.asList("1", "2", "3"));
-//        person.setAddress(Arrays.asList(new Address("a", "b"), new Address("c", "d")));
-//        System.out.println(xmlMapper.writeValueAsString(person));
-
-        Person person = xmlMapper.readValue("<Person><firstName>Alice</firstName><lastName>Bob</lastName><phoneNumbers><phoneNumbers>1</phoneNumbers><phoneNumbers>2</phoneNumbers><phoneNumbers>3</phoneNumbers></phoneNumbers><address><streetName>a</streetName><city>b</city></address><address><streetName>c</streetName><city>d</city></address></Person>", Person.class);
+    /**
+     * 将xml字符串读取为一个对象实例
+     *
+     * @throws JsonProcessingException json处理异常
+     */
+    private static void test1() throws JsonProcessingException {
+        Person person = MAPPER.readValue("<Person><firstName>Alice</firstName><lastName>Bob</lastName><phoneNumbers><phoneNumbers>1</phoneNumbers><phoneNumbers>2</phoneNumbers><phoneNumbers>3</phoneNumbers></phoneNumbers><address><streetName>a</streetName><city>b</city></address><address><streetName>c</streetName><city>d</city></address></Person>", Person.class);
         System.out.println(person);
     }
 }
