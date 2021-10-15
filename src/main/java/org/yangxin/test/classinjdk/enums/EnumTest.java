b219bb2708e3099519b251b2d09efaa31f309bbf
yangxin <1393003255@qq.com>
2021-10-15 18:28:42 +0800

    feat: 增加了枚举类的一些代码

---
+++ src/main/java/org/yangxin/test/classinjdk/enums/EnumTest.java
@@ -0,0 +1,16 @@
+package org.yangxin.test.classinjdk.enums;
+
+/**
+ * @author yangxin
+ * 2021/10/15 17:17
+ */
+public class EnumTest {
+
+    public static void main(String[] args) {
+        test1();
+    }
+
+    private static void test1() {
+        System.out.println(Light.GREEN.ordinal());
+    }
+}
