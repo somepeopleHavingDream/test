617ef970129b0acfe23a1906448036d8ec5b1d5d
yangxin <1393003255@qq.com>
2021-11-30 18:25:22 +0800

    feat: 增加了ArrayList的一些案例

--- src/main/java/org/yangxin/test/classinjdk/collection/list/ArrayListTest.java
+++ src/main/java/org/yangxin/test/classinjdk/collection/list/ArrayListTest.java
@@ -1,21 +1,43 @@
 package org.yangxin.test.classinjdk.collection.list;
 
-import java.util.Arrays;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 /**
  * @author yangxin
  * 2021/8/28 上午11:58
  */
-@SuppressWarnings("Java8CollectionRemoveIf")
+@SuppressWarnings({"Java8CollectionRemoveIf", "AlibabaUndefineMagicConstant"})
 public class ArrayListTest {
 
     public static void main(String[] args) {
-        test1();
+//        test1();
+        test2();
+    }
+
+    private static void test2() {
+        List<Row> rowList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            Row row = new Row();
+            Object[] valueArray = row.valueArray;
+            for (int j = 0; j < valueArray.length; j++) {
+                if ((j & 1) == 1) {
+                    valueArray[j] = "1";
+                } else {
+                    valueArray[j] = 0;
+                }
+            }
+
+            rowList.add(row);
+        }
+
+        System.out.println(rowList);
     }
 
     private static void test1() {
@@ -30,5 +52,13 @@ public class ArrayListTest {
         }
 
         System.out.println(list);
+    }
+
+    @Data
+    private static class Row implements Serializable {
+
+        private static final long serialVersionUID = 6537356899740364588L;
+
+        private final Object[] valueArray = new Object[5];
     }
 }
