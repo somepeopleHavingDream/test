780ed28ca51c7d38d84fd55fefec8cee3de9d1b9
yangxin <1393003255@qq.com>
2021-12-01 00:07:48 +0800

    feat: 增加ConcurrentHashMap的案例

---
+++ src/main/java/org/yangxin/test/classinjdk/collection/map/ConcurrentHashMapTest.java
@@ -0,0 +1,21 @@
+package org.yangxin.test.classinjdk.collection.map;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author yangxin
+ * 2021/11/30 下午11:16
+ */
+@SuppressWarnings("MismatchedQueryAndUpdateOfCollection")
+public class ConcurrentHashMapTest {
+
+    public static void main(String[] args) {
+        test1();
+    }
+
+    private static void test1() {
+        Map<String, String> map = new ConcurrentHashMap<>(256);
+        System.out.println(map.size());
+    }
+}
