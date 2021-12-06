59ef999f30f737c7f722753610c9f71f09034ba1
yangxin <1393003255@qq.com>
2021-12-06 18:25:30 +0800

    feat: 增加了FilterRegistrationBean的一些案例

---
+++ src/main/java/org/yangxin/test/spring/filterregistrationbean/Test2Filter.java
@@ -0,0 +1,20 @@
+package org.yangxin.test.spring.filterregistrationbean;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * @author yangxin
+ * 2021/12/6 11:44
+ */
+public class Test2Filter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        System.out.println("自定义过滤器filter2触发，拦截url：" + request.getRequestURI());
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
