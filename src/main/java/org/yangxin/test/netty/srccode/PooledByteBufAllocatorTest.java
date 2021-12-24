301066c6693c51b676da7e882c0c97e21cd5294a
yangxin <1393003255@qq.com>
2021-12-24 16:06:37 +0800

    feat: 研究netty怎么计算出块大小

---
+++ src/main/java/org/yangxin/test/netty/srccode/PooledByteBufAllocatorTest.java
@@ -0,0 +1,22 @@
+package org.yangxin.test.netty.srccode;
+
+/**
+ * @author yangxin
+ * 2021/12/24 15:44
+ */
+@SuppressWarnings("SameParameterValue")
+public class PooledByteBufAllocatorTest {
+
+    public static void main(String[] args) {
+        System.out.println(validateAndCalculateChunkSize(8192, 11));
+    }
+
+    private static int validateAndCalculateChunkSize(int pageSize, int maxOrder) {
+        int chunkSize = pageSize;
+        for (int i = maxOrder; i > 0; i--) {
+            chunkSize <<= 1;
+        }
+
+        return chunkSize;
+    }
+}
