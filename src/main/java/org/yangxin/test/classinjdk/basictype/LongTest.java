05a649233d7bc7a944fc54fdda2fffdc1c4a265b
yangxin <1393003255@qq.com>
2022-01-20 16:09:36 +0800

    feat: 增加了代码

--- src/main/java/org/yangxin/test/classinjdk/basictype/LongTest.java
+++ src/main/java/org/yangxin/test/classinjdk/basictype/LongTest.java
@@ -4,6 +4,7 @@ package org.yangxin.test.classinjdk.basictype;
  * @author yangxin
  * 2021/8/5 14:17
  */
+@SuppressWarnings({"WrapperTypeMayBePrimitive", "ConstantConditions"})
 public class LongTest {
 
     public static void main(String[] args) {
@@ -13,6 +14,9 @@ public class LongTest {
 
     private static void test2() {
         System.out.println(10000002515L % 30);
+
+        Long num = 0L;
+        System.out.println(Long.valueOf(num % 30).intValue());
     }
 
     private static void test1() {
