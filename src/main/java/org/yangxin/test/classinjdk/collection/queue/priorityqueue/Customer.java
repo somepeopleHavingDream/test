fe1580031dd5fc12bb6e6857f829aca5bf5ecfc3
yangxin <1393003255@qq.com>
2022-02-23 22:24:27 +0800

    feat: 增加了优先队列的使用案例

---
+++ src/main/java/org/yangxin/test/classinjdk/collection/queue/priorityqueue/Customer.java
@@ -0,0 +1,15 @@
+package org.yangxin.test.classinjdk.collection.queue.priorityqueue;
+
+import lombok.Data;
+
+/**
+ * @author yangxin
+ * 2022/2/23 22:04
+ */
+@Data
+public class Customer {
+
+    private Integer id;
+
+    private String name;
+}
---
+++ src/main/java/org/yangxin/test/classinjdk/collection/queue/priorityqueue/PriorityQueueTest.java
@@ -0,0 +1,31 @@
+package org.yangxin.test.classinjdk.collection.queue.priorityqueue;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Random;
+
+/**
+ * @author yangxin
+ * 2022/2/23 22:05
+ */
+@SuppressWarnings("AlibabaUndefineMagicConstant")
+public class PriorityQueueTest {
+
+    public static void main(String[] args) {
+        test1();
+    }
+
+    private static void test1() {
+        Random random = new Random();
+
+        // 优先队列自然排序示例
+        Queue<Integer> queue = new PriorityQueue<>();
+        for (int i = 0; i < 7; i++) {
+            queue.add(random.nextInt(100));
+        }
+        for (int i = 0; i < 7; i++) {
+            Integer poll = queue.poll();
+            System.out.println(poll);
+        }
+    }
+}
