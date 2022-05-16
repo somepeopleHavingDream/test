7c3b5f2d6728c94fab15ddebae96dea920307a2b
yangxin <1393003255@qq.com>
2022-05-16 10:27:46 +0800

    feat: 增加@ConditionalOnMissingBean注解的用法

---
+++ src/main/java/org/yangxin/test/spring/conditiononmissingbean/Test.java
@@ -0,0 +1,19 @@
+package org.yangxin.test.spring.conditiononmissingbean;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * @author yangxin
+ * 2022/5/16 10:15
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AutoConfig.class);
+
+        A a = context.getBean(A.class);
+        a.print();
+
+        context.close();
+    }
+}
