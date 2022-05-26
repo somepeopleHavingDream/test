43b1eea6741d8c505f8509988438a5647c484043
yangxin <1393003255@qq.com>
2022-05-26 18:23:44 +0800

    feat: redis实战，第二章，登录和cookie缓存

---
+++ src/main/java/org/yangxin/test/redis/action/Chapter02.java
@@ -0,0 +1,138 @@
+package org.yangxin.test.redis.action;
+
+import redis.clients.jedis.Jedis;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+/**
+ * hash login: -> 每个token对应了哪个用户
+ * zset recent: -> 每个token的最近时间
+ * zset viewd:token -> 该用户浏览某件商品的最近时间点
+ * zset viewd: -> 每件商品的个数
+ *
+ * @author yangxin
+ * 2022/5/26 17:30
+ */
+public class Chapter02 {
+
+    public static void main(String[] args) throws InterruptedException {
+        new Chapter02().run();
+    }
+
+    private void run() throws InterruptedException {
+        Jedis conn = new Jedis(ConfigConstant.HOST, ConfigConstant.PORT);
+        conn.auth(ConfigConstant.PASSWORD);
+        conn.select(15);
+
+        testLoginCookies(conn);
+    }
+
+    private void testLoginCookies(Jedis conn) throws InterruptedException {
+        System.out.println("\ntestLoginCookies");
+        String token = UUID.randomUUID().toString();
+
+        updateToken(conn, token, "username", "itemX");
+        System.out.println("We just logged-in/updated token: " + token);
+        System.out.println("For user: 'username'");
+        System.out.println();
+
+        System.out.println("What username do we get when we look-up that token?");
+        String r = checkToken(conn, token);
+        System.out.println(r);
+        System.out.println();
+        assert r != null;
+
+        System.out.println("Let's drop the maximum number of cookies to 0 to clean them out");
+        System.out.println("We will start a thread to do the cleaning, while we stop it later");
+
+        CleanSessionsThread thread = new CleanSessionsThread(0);
+        thread.start();
+        Thread.sleep(1000);
+        thread.quit();
+        Thread.sleep(2000);
+        if (thread.isAlive()) {
+            throw new RuntimeException("The clean sessions thread is still alive?!?");
+        }
+
+        long s = conn.hlen("login:");
+        System.out.println("The current number of sessions still available is: " + s);
+        assert s == 0;
+    }
+
+    private String checkToken(Jedis conn, String token) {
+        // 尝试获取并返回令牌对应的用户
+        return conn.hget("login:", token);
+    }
+
+    private void updateToken(Jedis conn, String token, String user, String item) {
+        // 获取当前时间戳
+        long timestamp = System.currentTimeMillis() / 1000;
+        // 维持令牌与已登录用户之间的映射
+        conn.hset("login:", token, user);
+        // 记录令牌最后一次出现的时间
+        conn.zadd("recent:", timestamp, token);
+        if (item != null) {
+            // 记录用户浏览过的商品
+            conn.zadd("viewd:" + token, timestamp, item);
+            // 移除旧的记录，只保留用户最近浏览过的25个商品
+            conn.zremrangeByRank("viewd:" + token, 0, -26);
+            conn.zincrby("viewd:", -1, item);
+        }
+    }
+
+    @SuppressWarnings({"BusyWait", "FieldMayBeFinal"})
+    private static class CleanSessionsThread extends Thread {
+
+        private Jedis conn;
+        private int limit;
+        private boolean quit;
+
+        public CleanSessionsThread(int limit) {
+            this.conn = new Jedis(ConfigConstant.HOST, ConfigConstant.PORT);
+            conn.auth(ConfigConstant.PASSWORD);
+            conn.select(15);
+
+            this.limit = limit;
+        }
+
+        public void quit() {
+            quit = true;
+        }
+
+        @Override
+        public void run() {
+            while (!quit) {
+                // 找出目前已有令牌的数量
+                long size = conn.zcard("recent:");
+                // 令牌数量未超过限制，休眠并在之后重新检查
+                if (size <= limit) {
+                    try {
+                        sleep(1000);
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                    continue;
+                }
+
+                // 获取需要移除的令牌Id
+                long endIndex = Math.min(size - limit, 100);
+                Set<String> tokenSet = conn.zrange("recent:", 0, endIndex - 1);
+                String[] tokens = tokenSet.toArray(new String[0]);
+
+                // 为那些将要被删除的令牌构建键名
+                List<String> sessionKeys = new ArrayList<>();
+                for (String token : tokens) {
+                    sessionKeys.add("viewd:" + token);
+                }
+
+                // 移除最旧的那些令牌
+                conn.del(sessionKeys.toArray(new String[0]));
+                conn.hdel("login:", tokens);
+                conn.zrem("recent:", tokens);
+            }
+        }
+    }
+}
