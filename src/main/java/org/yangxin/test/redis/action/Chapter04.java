8e96776f3807dab1e58f09e4b91ecbb9f5f56c97
yangxin <1393003255@qq.com>
2022-06-05 17:57:47 +0800

    feat: redis实战，第四章，非事务型流水线

--- src/main/java/org/yangxin/test/redis/action/Chapter04.java
+++ src/main/java/org/yangxin/test/redis/action/Chapter04.java
@@ -1,9 +1,11 @@
 package org.yangxin.test.redis.action;
 
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Pipeline;
 import redis.clients.jedis.Transaction;
 import redis.clients.jedis.Tuple;
 
+import java.lang.reflect.Method;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -30,6 +32,71 @@ public class Chapter04 {
 
         testListItem(conn, false);
         testPurchaseItem(conn);
+        testBenchmarkUpdateToken(conn);
+    }
+
+    public void testBenchmarkUpdateToken(Jedis conn) {
+        System.out.println("\n----- testBenchmarkUpdate -----");
+        benchmarkUpdateToken(conn, 5);
+    }
+
+    public void benchmarkUpdateToken(Jedis conn, int duration) {
+        try {
+            @SuppressWarnings("rawtypes")
+            Class[] args = new Class[]{
+                    Jedis.class, String.class, String.class, String.class};
+            Method[] methods = new Method[]{
+                    this.getClass().getDeclaredMethod("updateToken", args),
+                    this.getClass().getDeclaredMethod("updateTokenPipeline", args),
+            };
+            // 测试会分别执行update_token()函数和update_token_pipeline()函数
+            for (Method method : methods) {
+                // 设置计数器以及测试结束的条件
+                int count = 0;
+                long start = System.currentTimeMillis();
+                long end = start + (duration * 1000L);
+                while (System.currentTimeMillis() < end) {
+                    count++;
+                    // 调用两个函数的其中一个
+                    method.invoke(this, conn, "token", "user", "item");
+                }
+                // 计算函数的执行时长
+                long delta = System.currentTimeMillis() - start;
+                // 打印测试结果
+                System.out.println(
+                        method.getName() + ' ' +
+                                count + ' ' +
+                                (delta / 1000) + ' ' +
+                                (count / (delta / 1000)));
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void updateToken(Jedis conn, String token, String user, String item) {
+        long timestamp = System.currentTimeMillis() / 1000;
+        conn.hset("login:", token, user);
+        conn.zadd("recent:", timestamp, token);
+        if (item != null) {
+            conn.zadd("viewed:" + token, timestamp, item);
+            conn.zremrangeByRank("viewed:" + token, 0, -26);
+            conn.zincrby("viewed:", -1, item);
+        }
+    }
+
+    public void updateTokenPipeline(Jedis conn, String token, String user, String item) {
+        long timestamp = System.currentTimeMillis() / 1000;
+        Pipeline pipe = conn.pipelined();
+        pipe.multi();
+        pipe.hset("login:", token, user);
+        pipe.zadd("recent:", timestamp, token);
+        if (item != null){
+            pipe.zadd("viewed:" + token, timestamp, item);
+            pipe.zremrangeByRank("viewed:" + token, 0, -26);
+            pipe.zincrby("viewed:", -1, item);
+        }
+        pipe.exec();
     }
 
     public void testPurchaseItem(Jedis conn) {
@@ -38,9 +105,9 @@ public class Chapter04 {
 
         System.out.println("We need to set up just enough state so a user can buy an item");
         conn.hset("users:userY", "funds", "125");
-        Map<String,String> r = conn.hgetAll("users:userY");
+        Map<String, String> r = conn.hgetAll("users:userY");
         System.out.println("The user has some money:");
-        for (Map.Entry<String,String> entry : r.entrySet()){
+        for (Map.Entry<String, String> entry : r.entrySet()) {
             System.out.println("  " + entry.getKey() + ": " + entry.getValue());
         }
         assert r.size() > 0;
@@ -53,7 +120,7 @@ public class Chapter04 {
         assert p;
         r = conn.hgetAll("users:userY");
         System.out.println("Their money is now:");
-        for (Map.Entry<String,String> entry : r.entrySet()){
+        for (Map.Entry<String, String> entry : r.entrySet()) {
             System.out.println("  " + entry.getKey() + ": " + entry.getValue());
         }
         assert r.size() > 0;
@@ -61,7 +128,7 @@ public class Chapter04 {
         String buyer = "userY";
         Set<String> i = conn.smembers("inventory:" + buyer);
         System.out.println("Their inventory is now:");
-        for (String member : i){
+        for (String member : i) {
             System.out.println("  " + member);
         }
         assert i.size() > 0;
@@ -79,28 +146,28 @@ public class Chapter04 {
         long end = System.currentTimeMillis() + 10000;
 
         // 当买家的个人信息或者商品买卖市场出现变化而导致WatchError异常出现时，程序将进行重试，其中最大重试时间为10秒
-        while (System.currentTimeMillis() < end){
+        while (System.currentTimeMillis() < end) {
             // 对商品买卖市场以及买家的个人信息进行监视
             conn.watch("market:", buyer);
 
             // 检查买家想要购买的商品价格是否出现了变化，以及买家是否有足够的钱来购买这件商品
             double price = conn.zscore("market:", item);
             double funds = Double.parseDouble(conn.hget(buyer, "funds"));
-            if (price != lprice || price > funds){
+            if (price != lprice || price > funds) {
                 conn.unwatch();
                 return false;
             }
 
             // 先将买家支付的钱转移给卖家，然后将被购买的商品移交给买家
             Transaction trans = conn.multi();
-            trans.hincrBy(seller, "funds", (int)price);
-            trans.hincrBy(buyer, "funds", (int)-price);
+            trans.hincrBy(seller, "funds", (int) price);
+            trans.hincrBy(buyer, "funds", (int) -price);
             trans.sadd(inventory, itemId);
             trans.zrem("market:", item);
             List<Object> results = trans.exec();
             // null response indicates that the transaction was aborted due to
             // the watched key changing.
-            if (results == null){
+            if (results == null) {
                 // 如果买家的个人信息或者商品买卖市场在交易的过程中出现了变化，那么进行重试
                 continue;
             }
@@ -111,7 +178,7 @@ public class Chapter04 {
     }
 
     public void testListItem(Jedis conn, boolean nested) {
-        if (!nested){
+        if (!nested) {
             System.out.println("\n----- testListItem -----");
         }
 
@@ -122,7 +189,7 @@ public class Chapter04 {
         Set<String> i = conn.smembers("inventory:" + seller);
 
         System.out.println("The user's inventory has:");
-        for (String member : i){
+        for (String member : i) {
             System.out.println("  " + member);
         }
         assert i.size() > 0;
@@ -134,7 +201,7 @@ public class Chapter04 {
         assert l;
         Set<Tuple> r = conn.zrangeWithScores("market:", 0, -1);
         System.out.println("The market contains:");
-        for (Tuple tuple : r){
+        for (Tuple tuple : r) {
             System.out.println("  " + tuple.getElement() + ", " + tuple.getScore());
         }
         assert r.size() > 0;
@@ -151,7 +218,7 @@ public class Chapter04 {
             // 监视用户包裹发生的变化
             conn.watch(inventory);
             // 检查用户是否仍然持有将要被销售的商品
-            if (!conn.sismember(inventory, itemId)){
+            if (!conn.sismember(inventory, itemId)) {
                 // 如果指定的商品不在用户的包裹里面，那么停止对包裹键的监视并返回一个空值
                 conn.unwatch();
                 return false;
@@ -166,7 +233,7 @@ public class Chapter04 {
             List<Object> results = trans.exec();
             // null response indicates that the transaction was aborted due to
             // the watched key changing.
-            if (results == null){
+            if (results == null) {
                 // 用户的包裹已经发生了变化，重试
                 continue;
             }
