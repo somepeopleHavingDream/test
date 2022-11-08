f7c3938864f7604ab6c3aac76dd8f85b236f9d26
yangxin <1393003255@qq.com>
2022-11-08 23:28:09 +0800

    feat: 增加了ThreadLocalRandom类的用例

--- src/main/java/org/yangxin/test/jdk8/random/ThreadLocalRandomTest.java
+++ src/main/java/org/yangxin/test/jdk8/random/ThreadLocalRandomTest.java
@@ -1,25 +1,24 @@
 package org.yangxin.test.jdk8.random;
 
-import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * @author yangxin
- * 2022/1/12 18:27
+ * 2022/11/8 22:59
  */
-@SuppressWarnings("unused")
-public class RandomTest {
+public class ThreadLocalRandomTest {
 
     public static void main(String[] args) {
 //        test1();
     }
 
     private static void test1() {
-        Random random = new Random();
+        // ThreadLocalRandom能够解决多个线程发生的竞争问题
+        ThreadLocalRandom random = ThreadLocalRandom.current();
 
-        System.out.println(random.nextBoolean());
         // 随机生成0~50的随机数，不包括50
         System.out.println(random.nextInt(50));
         // 随机生成30~50的随机数，不包括50
-        System.out.println(random.nextInt(20) + 30);
+        System.out.println(random.nextInt(30, 50));
     }
 }
