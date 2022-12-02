46e7338e3cc40fe603e2ac00c87b82adcbe5f172
yangxin <1393003255@qq.com>
2022-12-02 11:18:59 +0800

    feat: 新增UUID的另一种使用案例

--- src/main/java/org/yangxin/test/id/UUIDTest.java
+++ src/main/java/org/yangxin/test/id/UUIDTest.java
@@ -12,5 +12,6 @@ public class UUIDTest {
 
     public static void main(String[] args) {
         System.out.println(UUID.randomUUID());
+        System.out.println(UUID.randomUUID().toString().replace("-", ""));
     }
 }
