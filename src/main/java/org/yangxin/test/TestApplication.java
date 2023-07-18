c9e1f6893726097b262583721278160f4c1f4a29
yangxin <1393003255@qq.com>
2023-07-18 18:29:49 +0800

    feat: 新增个banner.txt

--- src/main/java/org/yangxin/test/TestApplication.java
+++ src/main/java/org/yangxin/test/TestApplication.java
@@ -19,7 +19,8 @@ import org.yangxin.test.spring.event.OrderEvent;
 @SpringBootApplication
 @Slf4j
 @MapperScan("org.yangxin.test.mybatisplus")
-public class TestApplication implements CommandLineRunner {
+public class TestApplication {
+//public class TestApplication implements CommandLineRunner {
 
     private final ApplicationContext applicationContext;
     private final StringEncryptor stringEncryptor;
@@ -33,10 +34,10 @@ public class TestApplication implements CommandLineRunner {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(TestApplication.class, args);
 
-        // 测试事件
-        publishEvent(context);
-        // 测试log4j2日志
-        log4j2();
+//        // 测试事件
+//        publishEvent(context);
+//        // 测试log4j2日志
+//        log4j2();
     }
 
     private static void log4j2() {
@@ -52,23 +53,23 @@ public class TestApplication implements CommandLineRunner {
         context.publishEvent(new OrderEvent(context, new OrderEvent(context, "2")));
     }
 
-    @Override
-    public void run(String... args) {
-        Environment environment = applicationContext.getBean(Environment.class);
-
-        // 首先获取配置文件里的原始明文信息
-        String mysqlOriginPassword = environment.getProperty("spring.datasource.password");
-
-        // 加密
-        String mysqlEncryptedPassword1 = encrypt(mysqlOriginPassword);
-        String mysqlEncryptedPassword2 = encrypt("");
-
-        // 打印加密前后的结果对比
-        log.info("MySQL原始明文密码： [{}]", mysqlOriginPassword);
-        log.info("MySQL加密后密码： [{}]", mysqlEncryptedPassword1);
-        log.info("MySQL加密后密码： [{}]", mysqlEncryptedPassword2);
-        log.info("MySQL解密后密码： [{}]", decrypt("BQGsohp2tGtdi/wU5ck8wDyLGWQinHIZVV4DwG6sfV4aQMnsuYQUMvXvy0LieYzV"));
-    }
+//    @Override
+//    public void run(String... args) {
+//        Environment environment = applicationContext.getBean(Environment.class);
+//
+//        // 首先获取配置文件里的原始明文信息
+//        String mysqlOriginPassword = environment.getProperty("spring.datasource.password");
+//
+//        // 加密
+//        String mysqlEncryptedPassword1 = encrypt(mysqlOriginPassword);
+//        String mysqlEncryptedPassword2 = encrypt("");
+//
+//        // 打印加密前后的结果对比
+//        log.info("MySQL原始明文密码： [{}]", mysqlOriginPassword);
+//        log.info("MySQL加密后密码： [{}]", mysqlEncryptedPassword1);
+//        log.info("MySQL加密后密码： [{}]", mysqlEncryptedPassword2);
+//        log.info("MySQL解密后密码： [{}]", decrypt("BQGsohp2tGtdi/wU5ck8wDyLGWQinHIZVV4DwG6sfV4aQMnsuYQUMvXvy0LieYzV"));
+//    }
 
     /**
      * 加密
