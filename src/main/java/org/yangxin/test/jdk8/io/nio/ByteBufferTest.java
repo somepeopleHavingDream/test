ccaf0db77b29f7cf545ce1cffefab800f58c9bab
yangxin <1393003255@qq.com>
2023-08-10 15:07:23 +0800

    feat: 增加ByteBuffer的一些用例

--- src/main/java/org/yangxin/test/jdk8/io/nio/ByteBufferTest.java
+++ src/main/java/org/yangxin/test/jdk8/io/nio/ByteBufferTest.java
@@ -1,15 +1,33 @@
 package org.yangxin.test.jdk8.io.nio;
 
 import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.CharacterCodingException;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.StandardCharsets;
 
 /**
  * @author yangxin
  * 2021/8/17 10:54
  */
+@SuppressWarnings("unused")
 public class ByteBufferTest {
 
-    public static void main(String[] args) {
-        test1();
+    public static void main(String[] args) throws CharacterCodingException {
+//        test1();
+        test2();
+    }
+
+    private static void test2() throws CharacterCodingException {
+        String str = "{\"char\":\"笢\",\"direct\":false,\"double\":1.7114893826238545e+214,\"float\":2.5233746e-18,\"int\":909325612,\"long\":2480473404756814300,\"readOnly\":true,\"short\":8762}";
+        byte[] body = str.getBytes(StandardCharsets.UTF_8);
+        ByteBuffer byteBuffer = ByteBuffer.wrap(body);
+
+        Charset charset = StandardCharsets.UTF_8;
+        CharsetDecoder decoder = charset.newDecoder();
+        CharBuffer charBuffer = decoder.decode(byteBuffer);
+        System.out.println(charBuffer);
     }
 
     private static void test1() {
