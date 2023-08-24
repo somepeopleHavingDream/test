bfe017ef29c6cc39478e1ac72b005909bd192c38
yangxin <1393003255@qq.com>
2023-08-24 20:40:04 +0800

    feat: 增加对Spring的StandardAnnotationMetadata的研究案例

---
+++ src/main/java/org/yangxin/test/spring/annotationmetadata/MyClass.java
@@ -0,0 +1,14 @@
+package org.yangxin.test.spring.annotationmetadata;
+
+/**
+ * @author yangxin
+ * 2023/8/23 22:20
+ */
+@SuppressWarnings("unused")
+@MyAnnotation(value = "Hello, Annotation!")
+public class MyClass {
+
+    public void someMethod() {
+        System.out.println("someMethod");
+    }
+}
