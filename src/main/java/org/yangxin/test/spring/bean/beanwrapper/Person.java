5654a78e3a63e6873595a4b9a835c69fca0241ca
yangxin <1393003255@qq.com>
2023-09-17 15:34:19 +0800

    feat: 增加了BeanWrapper的使用案例

---
+++ src/main/java/org/yangxin/test/spring/bean/beanwrapper/Person.java
@@ -0,0 +1,14 @@
+package org.yangxin.test.spring.bean.beanwrapper;
+
+import lombok.Data;
+
+/**
+ * @author yangxin
+ * 2023/9/17 15:26
+ */
+@Data
+public class Person {
+
+    private String name;
+    private int age;
+}
