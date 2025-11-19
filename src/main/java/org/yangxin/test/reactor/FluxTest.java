a6f3beb392238897e83473969d982cb25a512198
yangxin <1393003255@qq.com>
2025-11-19 14:30:37 +0800

    feat: 增加了一些测试用例

--- src/main/java/org/yangxin/test/reactor/FluxTest.java
+++ src/main/java/org/yangxin/test/reactor/FluxTest.java
@@ -9,11 +9,11 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-@SuppressWarnings("CommentedOutCode")
+@SuppressWarnings({"CommentedOutCode", "unused"})
 public class FluxTest {
     public static void main(String[] args) {
-        test1();
-//        test2();
+//        test1();
+        test2();
 //        test3();
 //        test4();
     }
